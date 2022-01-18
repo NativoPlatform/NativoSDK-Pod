baade9eff035381f3f36ca319394038cf24ca286
Matt Murray <mattm.trinsic@gmail.com>
2022-01-18 13:22:11 -0800

    6.0.6-alpha.1

--- NativoSDK.xcframework/ios-arm64_i386_x86_64-simulator/NativoSDK.framework/Headers/NativoSDK.h
+++ NativoSDK.xcframework/ios-arm64_i386_x86_64-simulator/NativoSDK.framework/Headers/NativoSDK.h
@@ -31,7 +31,7 @@ extern const unsigned char NativoSDKVersionString[];
  
  Refer to https://sdk.nativo.com for guides and documentation.
  
- __Version__: 6.0.5
+ __Version__: 6.0.6
  
  */
 NS_ASSUME_NONNULL_BEGIN
